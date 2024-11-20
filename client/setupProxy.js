2b0170be144bfd8e9e3ca2a801e557ff32b53278
wonjoo88 <wonjoo88@revelogis.com>
2024-11-21 08:42:16 +0900

    proxy err 2

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/',
+    '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3500', // 서버의 URL
+      target: 'http://localhost:3500/api', // 서버의 URL
       changeOrigin: true,
     })
   );
